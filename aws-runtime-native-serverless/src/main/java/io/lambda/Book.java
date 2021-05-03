e41af224bea67ed0f1c5a73d84dd3b38e1236601
Anton Kurako <goodforgod.dev@gmail.com>
2021-05-03 15:38:48 +0300

    AwsRuntimeNativeServerless application.yml, logback.xml added Lombok integrated

--- aws-runtime-native-serverless/src/main/java/io/lambda/Book.java
+++ aws-runtime-native-serverless/src/main/java/io/lambda/Book.java
@@ -1,27 +1,18 @@
 package io.lambda;
 
 import io.micronaut.core.annotation.TypeHint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * @author Anton Kurako (GoodforGod)
  * @since 21.3.2021
  */
 @TypeHint(value = { Book.class }, accessType = { TypeHint.AccessType.ALL_PUBLIC })
+@Getter
+@AllArgsConstructor
 public class Book {
 
     private final String id;
     private final String name;
-
-    public Book(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
--- aws-runtime-native-serverless/src/main/java/io/lambda/User.java
+++ aws-runtime-native-serverless/src/main/java/io/lambda/User.java
@@ -1,30 +1,18 @@
 package io.lambda;
 
 import io.micronaut.core.annotation.TypeHint;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Anton Kurako (GoodforGod)
  * @since 21.3.2021
  */
 @TypeHint(value = { User.class }, accessType = { TypeHint.AccessType.ALL_PUBLIC })
+@Getter
+@Setter
 public class User {
 
     private String id;
     private String name;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
