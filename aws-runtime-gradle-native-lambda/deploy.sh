9ee0b247db0500ff2de5576bae4e7844d1a3331c
Anton Kurako <goodforgod.dev@gmail.com>
2021-06-29 10:13:27 +0300

    Aws Runtime renamed and structure refactored

---
+++ aws-runtime-gradle-native-lambda/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker build -t aws-runtime-gradle-native-lambda .
+mkdir -p build
+docker run --rm --entrypoint cat aws-runtime-gradle-native-lambda /home/application/function.zip > build/function.zip
+
+# sam local start-api -t sam.yaml -p 3000
