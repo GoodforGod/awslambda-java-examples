485f3a956f892a34562a44d3a37f5613bdcba9ef
Anton Kurako <goodforgod.dev@gmail.com>
2021-11-03 01:42:37 +0300

    Minor dependency updated and Test update

--- simplelambda-gradle-micronaut/src/test/java/io/goodforgod/simplelambda/micronaut/HelloWorldLambdaTests.java
+++ simplelambda-gradle-micronaut/src/test/java/io/goodforgod/simplelambda/micronaut/HelloWorldLambdaTests.java
@@ -3,10 +3,11 @@ package io.goodforgod.simplelambda.micronaut;
 import io.goodforgod.aws.simplelambda.handler.EventHandler;
 import io.goodforgod.aws.simplelambda.handler.impl.BodyEventHandler;
 import io.goodforgod.aws.simplelambda.handler.impl.InputEventHandler;
+import io.goodforgod.aws.simplelambda.micronaut.MicronautBodyLambdaEntrypoint;
 import io.goodforgod.aws.simplelambda.runtime.EventContext;
+import io.goodforgod.aws.simplelambda.runtime.RuntimeContext;
 import io.goodforgod.aws.simplelambda.utils.InputStreamUtils;
 import io.goodforgod.aws.simplelambda.utils.SubscriberUtils;
-import io.micronaut.context.ApplicationContext;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.UUID;
@@ -21,8 +22,8 @@ import org.junit.jupiter.api.Test;
 class HelloWorldLambdaTests extends Assertions {
 
     @Test
-    void gatewayEvent() {
-        try (final ApplicationContext context = ApplicationContext.run()) {
+    void gatewayEvent() throws Exception {
+        try (RuntimeContext context = new MicronautBodyLambdaEntrypoint().getRuntimeContext()) {
             final EventHandler handler = context.getBean(BodyEventHandler.class);
 
             final String eventAsString = "{\"httpMethod\":\"GET\",\"queryStringParameters\":{\"from\":\"one\",\"to\":\"ten\"},\"isBase64Encoded\":false,\"body\":\"{\\\"name\\\":\\\"bob\\\"}\"}";
@@ -39,8 +40,8 @@ class HelloWorldLambdaTests extends Assertions {
     }
 
     @Test
-    void directEvent() {
-        try (final ApplicationContext context = ApplicationContext.run()) {
+    void directEvent() throws Exception {
+        try (RuntimeContext context = new MicronautBodyLambdaEntrypoint().getRuntimeContext()) {
             final EventHandler handler = context.getBean(InputEventHandler.class);
 
             final String eventAsString = "{\"name\":\"bob\"}";
